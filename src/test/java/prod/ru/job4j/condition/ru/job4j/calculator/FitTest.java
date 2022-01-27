a65f99bc4bd5b108f996a845eeefb27baae40baf
Aleksandr <losas20@yandex.ru>
2022-01-27 18:51:29 +0600

    2. Тесты для задачи "идеальный вес"2

---
+++ src/test/java/prod/ru/job4j/condition/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,16 @@
+package prod.ru.job4j.condition.ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+    }
+
+    @Test
+    public void womanWeight() {
+    }
+}
