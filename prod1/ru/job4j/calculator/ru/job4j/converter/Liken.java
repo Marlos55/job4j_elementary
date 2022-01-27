eba086249b96611908f89ba468b4981741985229
Aleksandr <losas20@yandex.ru>
2022-01-28 00:17:48 +0600

    0. Операции сравнения

---
+++ prod1/ru/job4j/calculator/ru/job4j/converter/Liken.java
@@ -0,0 +1,17 @@
+package prod1.ru.job4j.calculator.ru.job4j.converter;
+
+public class Liken {
+    public static void main(String[] args){
+        int first = 10;
+        int second = 9;
+
+        boolean result1 = first > second;
+        System.out.println(result1);
+
+        boolean result2 = first < second;
+        System.out.println(result2);
+
+        boolean result3 = first == second;
+        System.out.println(result3);
+    }
+}
